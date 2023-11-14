26827708bad43b8b2977626ec74381ccee58058e
Kamyla ferreira <105284926+Kamylaferreira@users.noreply.github.com>
2023-11-14 09:15:15 -0300

    ok

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ for(let contador = 0;contador < listaDeTeclas.length;contador++){
       const tecla = listaDeTeclas[contador];
       const efeito = tecla.classList[1];
       const idAudio = `#som_${efeito}`;
-    tecla.onclick = function(){
+      tecla.onclick = function(){
            tocaSom(idAudio);
 
-      };
+      }
       
 }
